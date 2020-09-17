abd62218af38a8fa0d071028617db79fe62e8c61
Jörg Bartnick <joerg@bartnick.eu>
2020-09-17 22:55:37 +0200

    Fix the smoke test to run an actual "Hello World!" programm

--- tests/smoke.c
+++ tests/smoke.c
@@ -8,8 +8,8 @@
 int main() {
     HtmlState *state = html_state();
     HtmlExecutionContext *context = html_context(HTML_TAPE_SIZE);
-    HtmlInstruction *instruction = html_parse_string("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.++"
-                                                               "+.------.--------.>>+.>++.");
+    HtmlInstruction *instruction = html_parse_string("tttttttthLtttthLttLtttLtttLtHHHHmlLtLtLmLLthHlHmlLLTLmmmTtttttttTTtttTLLTHmTHTtt"
+                                                     "tTmmmmmmTmmmmmmmmTLLtTLttT");
     html_add(state, instruction);
     html_execute(state->root, context);
     html_destroy_context(context);
